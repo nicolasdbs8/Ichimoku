2668e44eef771b63f26bd37e9fccbefd89c5b672
nicolasdbs8 <nicolas.debesse@gmail.com>
2026-03-03 09:56:14 +0100

    Update utils.py

--- indicators/utils.py
+++ indicators/utils.py
@@ -1,1 +1,21 @@
+from __future__ import annotations
 
+import pandas as pd
+
+
+def kumo_top(span_a: pd.Series, span_b: pd.Series) -> pd.Series:
+    return pd.concat([span_a, span_b], axis=1).max(axis=1)
+
+
+def kumo_bottom(span_a: pd.Series, span_b: pd.Series) -> pd.Series:
+    return pd.concat([span_a, span_b], axis=1).min(axis=1)
+
+
+def kijun_slope(kijun: pd.Series, n: int = 10) -> pd.Series:
+    return kijun - kijun.shift(n)
+
+
+def kumo_thickness_pct(span_a: pd.Series, span_b: pd.Series, close: pd.Series) -> pd.Series:
+    top = kumo_top(span_a, span_b)
+    bot = kumo_bottom(span_a, span_b)
+    return (top - bot) / close
